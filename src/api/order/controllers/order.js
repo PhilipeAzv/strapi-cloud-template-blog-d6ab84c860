a1fdfeb215d21acfbcd2574c0ad13b468b76bda6
Philipe Azevedo <109996175+PhilipeAzv@users.noreply.github.com>
2024-06-08 13:00:36 -0300

    Update order.js

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -35,7 +35,7 @@ module.exports = createCoreController('api::order.order', ({ strapi }) => ({
       async function setPaymentStatus(){
         const paymentUpdateData = await fetchPaymentData().then(res=>res).then(data=>data).catch(err=>err)
         const {external_id , status } = paymentUpdateData
-        
+        return {external_id, status}
         if(status == 'cancelled'){
           const order = await strapi.db.query('api::order.order').findOne({
             where: {
