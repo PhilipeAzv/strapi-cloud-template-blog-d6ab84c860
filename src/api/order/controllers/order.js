1054f40cf201524178081cffcf00e9f3f6885b4a
Philipe Azevedo <109996175+PhilipeAzv@users.noreply.github.com>
2024-06-10 11:42:07 -0300

    Update order.js

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -33,7 +33,8 @@ module.exports = createCoreController('api::order.order', ({ strapi }) => ({
       }
 
       async function setPaymentStatus(){
-        const paymentUpdateData = await fetchPaymentData().then(res=>res).then(data=>data).catch(err=>err)
+        //const paymentUpdateData = await fetchPaymentData().then(res=>res).then(data=>data).catch(err=>err)
+        const paymentUpdateData = ctx.request.body
         const {external_reference , status } = paymentUpdateData
         if(status == 'approved'){
           const order = await strapi.db.query('api::order.order').findOne({
